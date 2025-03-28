9e0faabb5c43478eae3d1f41ef5063507b89c2dc
Surendra Singh <surendra.singh@scalableapps.io>
2025-03-28 09:50:58 -0400

    initial version

--- leetcode/339-nested-list-weight-sum.py
+++ leetcode/339-nested-list-weight-sum.py
@@ -50,16 +50,3 @@
 
 
 
-class Solution:
-    def depthSum(self, nestedList: List[NestedInteger]) -> int:
-        def dfs(nestedList,depth):
-            total=0
-            for i in nestedList:
-                if i.isInteger():
-                    total +=i.getInteger() * depth
-                else:
-                    total +=dfs(i.getList(),depth+1)
-            return total
-        return dfs(nestedList,1)
-
-
