aa309080d445f0c3a96f6919a0324c2e1afbf9b1
Surendra Singh <surendra.singh@scalableapps.io>
2025-04-17 09:18:08 -0400

    initial version

--- leetcode/1091-shortest-path-in-binary-matrix.py
+++ leetcode/1091-shortest-path-in-binary-matrix.py
@@ -1,5 +1,5 @@
 from collections import deque
-
+from collections import  defaultdict
 
 class Solution:
     def shortestPathBinaryMatrix(self, grid: List[List[int]]) -> int:
@@ -8,27 +8,60 @@ class Solution:
         if grid[-1][-1] == 1:
             return -1
         directions = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
-        queue = deque([(0, 0)])
+        queue = deque([(0, 0),0])
         visited = {(0, 0)}
         distance = 1
         n = len(grid)
         target = (n - 1, n - 1)
+        bfs_levels=defaultdict(list)
         while queue:
             level_size = len(queue)
-            for _ in range(level_size):
-                cell = queue.popleft()
-                if cell == target:
-                    return distance
-                for dx, dy in directions:
+            cell,level = queue.popleft()
+            bfs_levels[level].append(cell)
+            #for _ in range(level_size):
+
+              #  if cell == target:
+                 #   return distance
+            for dx, dy in directions:
                     current_x = cell[0] + dx
                     current_y = cell[1] + dy
                     if 0 <= current_x < len(grid) and 0 <= current_y < len(grid[0]):
                         if (current_x, current_y) not in visited and grid[current_x][current_y] != 1:
-                            queue.append((current_x, current_y))
-                            visited.add((current_x, current_y))
-            distance += 1
+                            queue.append((current_x, current_y),level+1)
+                            visited.add((current_x, current_y),level+1)
+
+        for key in range(len(bfs_levels)):
+            if bfs_levels[key] == target:
+                return key
         return -1
 
+    def shortestPathBinaryMatrix_single_pass(self, grid: List[List[int]]) -> int:
+        if grid[0][0] == 1:
+            return -1
+        if grid[-1][-1] == 1:
+            return -1
+        directions = [(1, 0), (0, 1), (-1, 0), (0, -1), (1, 1), (1, -1), (-1, 1), (-1, -1)]
+        queue = deque([((0, 0), 1)])
+        visited = {(0, 0)}
+        n = len(grid)
+        target = (n - 1, n - 1)
+        bfs_levels = defaultdict(list)
+        while queue:
+            cell, level = queue.popleft()
+            bfs_levels[level].append(cell)
+            for dx, dy in directions:
+                current_x = cell[0] + dx
+                current_y = cell[1] + dy
+                child_node = ((current_x, current_y), level + 1)
+                if 0 <= current_x < len(grid) and 0 <= current_y < len(grid[0]):
+                    if child_node[0] not in visited and grid[current_x][current_y] != 1:
+                        queue.append(child_node)
+                        visited.add(child_node[0])
+
+        for key in range(1, len(bfs_levels) + 1):
+            if target in bfs_levels[key]:
+                return key
+        return -1
 
 
 
